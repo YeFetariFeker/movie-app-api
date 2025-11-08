149f246af1d1fd9c3ac3292511d9184eeac56f87
botigi9404 <126343755+botigi9404@users.noreply.github.com>
2025-11-07 19:50:48 -0600

    added helmet and cors to server.js; built congig/dbconfig.js

--- server.js
+++ server.js
@@ -3,4 +3,26 @@ const express = require('express')
 const server = express()
 const PORT = process.env.PORT || 3000
 
+/* Handel security */
+const helmet = require('helmet')
+const cors = require('cors')
+
+/* Adds the layer security that we need basically for imgs, static javaScripit or CSS files */
+/* Configuring helmet */
+//server.use(helmet())  // we are configuring helmet and we are  not going to use it 
+server.use(helmet.contentSecurityPolicy({
+    useDefaults: true,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    directives: {
+        "img-src": ["'self'", "https: data"],
+        "scriptSrc": ["'self'", "cdn.jsdelivr.net"]
+    }
+}))
+
+server.use(cors())
+server.use(express.json)
+server.use(express.urlencoded({extended: true}))
+
+
 server.listen(PORT, ()=> console.log(`My Movie API is now showing `))
